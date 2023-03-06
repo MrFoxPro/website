34f937efa4d3f76f1987eadbf45dc494325d7530
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2023-03-06 20:20:07 +0500

    workaround for inlining css in html-only pages

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -1,25 +1,58 @@
-import { generateHydrationScript, renderToStream } from 'solid-js/web'
-import { escapeInject as escape, dangerouslySkipEscape as skip, stampPipe } from 'vite-plugin-ssr'
-import type { PageContextBuiltIn } from 'vite-plugin-ssr'
-import Favicons from './favicons.html?raw'
+import { generateHydrationScript, renderToStringAsync } from 'solid-js/web'
+import { escapeInject as escape, dangerouslySkipEscape as skip } from 'vite-plugin-ssr'
 import { prepare_page } from './common'
+import type { PageContextBuiltIn, InjectFilterEntry } from 'vite-plugin-ssr'
+import Favicons from './favicons.html?raw'
+import type { ViteDevServer } from 'vite'
+import { readFile } from 'fs/promises'
+import Path from 'path'
 
 const hydrationScript = generateHydrationScript()
-const head = [Favicons]
-
-export function render(ctx: PageContextBuiltIn) {
+const viteDevServer = globalThis.__vite_plugin_ssr['globalContext.ts'].viteDevServer as ViteDevServer
+const viteConfig = globalThis['__vite_plugin_ssr']['globalContext.ts']['config']
+export async function render(
+   ctx: PageContextBuiltIn & { _isHtmlOnly: boolean; __getPageAssets: () => Promise<InjectFilterEntry[]> },
+) {
    const {
       exports: { hydrate },
    } = ctx
 
    const Comp = prepare_page(ctx)
-   const { pipe } = renderToStream(Comp)
+   const body = await renderToStringAsync(Comp)
 
-   stampPipe(pipe, 'node-stream')
+   const head = [Favicons]
+   const assets_to_skip = new Set()
 
    if (hydrate) head.push(hydrationScript)
 
-   return escape`
+   if (ctx._isHtmlOnly) {
+      const assets = await ctx.__getPageAssets()
+      for (const a of assets) {
+         if (a.assetType != 'style') continue
+         let content: string
+         if (viteDevServer) {
+            const mod = await viteDevServer.moduleGraph.getModuleByUrl(a.src)
+            content = mod?.transformResult.code
+         } else
+            content = await readFile(Path.join(viteConfig.root, viteConfig.build.outDir, a.src), {
+               encoding: 'utf8',
+            })
+         head.unshift(`<style>${content}</style>`)
+         // a.inject = false - will not work here
+         assets_to_skip.add(a.src)
+      }
+   }
+
+   const injectFilter = (assets: InjectFilterEntry[]) => {
+      if (!ctx._isHtmlOnly) return
+      for (const a of assets) {
+         if (assets_to_skip.has(a.src)) {
+            a.inject = false
+         }
+      }
+   }
+
+   const documentHtml = escape`
          <!DOCTYPE html>
             <html lang="en">
             <head>
@@ -27,7 +60,11 @@ export function render(ctx: PageContextBuiltIn) {
                <title>foxpro website</title>
                ${skip(head.join('\n'))}
             </head>
-            <body>${pipe}</body>
+            <body>${skip(body)}</body>
          </html>
       `
+   return {
+      documentHtml,
+      injectFilter,
+   }
 }
